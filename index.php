981a82be74ecab8fb6319aa0c62060d726e454f1
Pranay Das <43801497+pranaydas252@users.noreply.github.com>
2018-10-18 13:03:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <head>
+    <title>Freepics: Free stock photos for your project</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <!-- font awesome css -->
+    <link href="css/font-awesome.min.css" type="text/css" rel="stylesheet" media="all" />
+    <!-- bootstrap css -->
+    <link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all" />
+    <!-- custom css -->
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="all" />
+</head>
+    
+</head>
+
+<body>
+    <div class="jumbotron text-center">
+        <img src="images/logo.png">
+        <h1>Freepics</h1>
+        <p>A place where you can find Royalty-Free, Non-Copyrighted and High-Definition stock photos for your Projects.</p>  
+        <form class="form" id="form">
+            <input class="form-control" type="text" id="search" placeholder="Search for photos">
+            <button class="btn" type="submit" id="button"><span class="fa fa-search">&nbsp;&nbsp;&nbsp;SEARCH</span></button>
+        </form>
+
+    </div> 
+        <div id="photos" class="grid text-center">
+        
+        </div>
+        
+        
+        <!-- Footer -->
+        <footer id="footer">
+            <div class="container py-5">
+                <div class="row text-center">
+                    <div class="col-md-12">
+                        <h4>Made with <span class="fa fa-heart"></span> by Pranay.</h4>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <div class="col-md-12 social">
+                        <h2><a href="https://www.instagram.com/pranaydas252" target="_blank"><span class="fa fa-instagram"></span></a><a href="https://github.com/pranaydas252" target="_blank"><span class="fa fa-github"></span></a></h2>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- /.footer -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="js/bootstrap.js"></script>
+</body>
+
+</html>
+
